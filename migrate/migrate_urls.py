daf4915949e7aace2c08e45e793c2b98cc826ba7
Andrei Petre <p31andrei@gmail.com>
2014-07-08 15:36:42 +0000

    Migrate shortened URLs from db to expanded ones. #18

--- migrate/migrate_urls.py
+++ migrate/migrate_urls.py
@@ -2,12 +2,18 @@ from helpers.helpers import unshorten_url
 
 def migrate_urls(db, col):
     '''Expand the URLs as much as possible and update it back, upsert.'''
-    total = db[col].count()
     cursor = db[col].find({'entities.urls.expanded_url': {'$exists': True}})
+    total = cursor.count()
+    print 'Total urls to go', total
     for i, tweet in enumerate(cursor):
-        url = tweet['entities']['urls']['expanded_url']
-        uurl = unshorten_url(url)
-        #tweet['entities']['urls']['expanded_url'] = uurl
-        print url
-        print uurl
-        break
+        for url in tweet['entities']['urls']:
+            u = url['expanded_url']
+            print u
+            uu = unshorten_url(u)
+            url['expanded_url'] = uu
+            print uu
+        db[col].update({'_id': tweet['_id']}, tweet, upsert=True)
+        if (i+1) % 100 == 0:
+            print '---------------'
+            print 'At migration step ' + str(i) + ' / ' + str(total)
+            print '---------------'
